292696739078c78535a5720234fadd5bf00a486a
Maduz0097 <aymperera@gmail.com>
2022-05-12 07:37:06 +0530

    make it better

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/api/v1/sales", saleRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 const start = async () => {
   try {
